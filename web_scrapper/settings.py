b84031cfa46ff7607e035686c447861d01d35b9e
umesh raut <rautumesh34@gmail.com>
2020-03-11 17:11:42 +0530

    get redy for heroku

--- web_scrapper/settings.py
+++ web_scrapper/settings.py
@@ -123,4 +123,4 @@ STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
 
 STATIC_URL = '/static/'
 
-django_heroku.settings(licals())
+django_heroku.settings(locals()                                                                                                                                     )
